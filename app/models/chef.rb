75ad9493728ec21acf743f2d7d0d75edfcf7093d
swiches <donasare@live.com>
2022-02-21 14:35:10 +0000

    chef test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,8 @@
 class Chef < ActiveRecord::Base 
-
+	before_save { self.email = email.downcase}
+ 	validates :chefname, presence: true, length:{ minimun:3, maximum: 40 }
+ 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+ 	validates :email, presence: true, length: { maximum: 105 },
+ 										uniqueness: { case_sensitive: false }, 
+ 										format: { with: VALID_EMAIL_REGEX }
 end 
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -12,8 +12,60 @@ class RecipeTest < ActiveSupport::TestCase
     end 
 
     test "name should be present" do
-        @recipe.name = ""
+        @recipe.name = " "
         assert_not @recipe.valid? 
     end
+
+    test "name length should not be too long" do 
+        @recipe.name ="a" *101
+        assert_not @recipe.valid?
+    end 
+
+    test "name length should not be too short" do 
+        @recipe.name = "aaaa"
+        assert_not @recipe.valid?
+    end 
+
+
+    test "summary should be present" do 
+        @recipe.summary = ""
+        assert_not @recipe.valid? 
+
+    end 
+
+
+    test "summary length should not be too short" do 
+         @recipe.summary = "a"*9
+         assert_not @recipe.valid? 
+
+    end 
+
+
+    test "summary length should not be too long" do 
+        @recipe.summary = "a"*151
+        assert_not @recipe.valid? 
+
+    end 
  
+    test "description must be present" do 
+        @recipe.description = ""
+        assert_not @recipe.valid?
+
+    end
+
+
+    test "description length should not be too long"  do 
+        @recipe.description = "a"*501
+        assert_not @recipe.valid? 
+    end 
+
+
+    test "description length should not be too short "  do 
+        @recipe.description = "a" * 19
+        assert_not @recipe.valid?
+
+    end 
+
+
+
 end  
