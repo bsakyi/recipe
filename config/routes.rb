54473d7bf97ca5613d4ae0424b52b1dd8be8ab2b
swiches <donasare@live.com>
2022-03-02 12:59:29 +0000

    association

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 root to: "pages#home"
-
 get '/home' => 'pages#home'
+
+#get '/recipes', to: 'recipes#index'
+#get 'recipes/new', to: 'recipe#new', as: 'new_recipe'
+#post 'recipes', to: 'recipes#create'
+#get 'recipes/:id/edit', to: 'recipe#edit', as: 'edit_recipe'
+#patch 'recipes/:id', to: 'recipe#update'
+#get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+#delete 'recipe/:id', to: 'recipes#destroy'
+
+resources :recipes
+
 end
